f08a259a86f9ccfaa9f43ff0e8f6fbe59880fbad
Bastin Gonzalez <bastingonzalez16@gmail.com>
2022-03-29 21:08:22 -0700

    new commit
    hola

--- models/perros.model.js
+++ models/perros.model.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 const {Schema} = mongoose;
-//comentario
+//api
 const PerrosSchema = new Schema({
   nombre: {type: String, require: true},
   raza:{type: String, required: true},
